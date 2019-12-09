abcd854df0f6e456c329cb76663b3ca4086692cd
Nikita Maslov <developer.nikita.maslov@gmail.com>
2019-12-09 18:49:02 +0300

    Implement saving to file and copying to clipboard

--- app/src/main/java/com/nikitamaslov/textlytic/recognition/RecognitionActivity.kt
+++ app/src/main/java/com/nikitamaslov/textlytic/recognition/RecognitionActivity.kt
@@ -2,13 +2,22 @@ package com.nikitamaslov.textlytic.recognition
 
 import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.ml.vision.FirebaseVision
 import com.nikitamaslov.textlytic.R
 import com.nikitamaslov.textlytic.util.Response
 import kotlinx.android.synthetic.main.content_recognition.*
 import kotlinx.android.synthetic.main.header_recognition.*
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.widget.Toast
+import java.io.IOException
+import java.io.OutputStreamWriter
+
+
+private const val CLIPBOARD_LABEL = "textlytic"
+private const val TEXT_FILE_NAME = "textlytic.txt"
 
 class RecognitionActivity : AppCompatActivity() {
 
@@ -18,6 +27,7 @@ class RecognitionActivity : AppCompatActivity() {
 
     private lateinit var imageUri: Uri
     private lateinit var recognizer: Recognizer
+    private var recognized: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +40,7 @@ class RecognitionActivity : AppCompatActivity() {
         initImageUri()
 
         recognized_image_view.setImageURI(imageUri)
+        setListeners()
 
         initDependencies()
         recognize()
@@ -46,6 +57,11 @@ class RecognitionActivity : AppCompatActivity() {
         this.imageUri = Uri.parse(rawImageUri)
     }
 
+    private fun setListeners() {
+        copy_to_clipboard_btn.setOnClickListener { copyToClipboard() }
+        save_to_file_btn.setOnClickListener { saveToFile() }
+    }
+
     private fun initDependencies() {
         this.recognizer = FirebaseRecognizer(applicationContext, FirebaseVision.getInstance())
     }
@@ -53,10 +69,42 @@ class RecognitionActivity : AppCompatActivity() {
     private fun recognize() {
         val callback: (Response<String>) -> Unit = {
             recognized_et.text = when (it) {
-                is Response.Success -> it.content
-                is Response.Failure -> "error"
+                is Response.Success -> it.content.also { str -> this.recognized = str }
+                is Response.Failure -> getString(R.string.error_recognition)
             }
         }
         recognizer.recognize(imageUri, callback)
     }
+
+    /*
+     * ACTION region
+     */
+
+    private fun copyToClipboard() {
+        if (recognized == null) return
+        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clip = ClipData.newPlainText("", recognized)
+        clipboard.setPrimaryClip(clip)
+        Toast.makeText(this, R.string.notification_copied_to_clipboard, Toast.LENGTH_LONG).show()
+    }
+
+    private fun saveToFile() {
+        if (recognized == null) {
+            return
+        }
+        try {
+            val outputStreamWriter =
+                OutputStreamWriter(openFileOutput(TEXT_FILE_NAME, Context.MODE_PRIVATE))
+            outputStreamWriter.write(recognized!!)
+            outputStreamWriter.close()
+            Toast.makeText(this, R.string.notification_saved_to_file, Toast.LENGTH_LONG).show()
+        } catch (e: IOException) {
+            Toast.makeText(this, R.string.error_save_to_file, Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    /*
+     * end of region
+     */
 }
